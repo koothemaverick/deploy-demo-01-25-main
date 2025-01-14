d9234af441660ebcfd8208ca17705c9f60d2a489
KOOTHEMAVERICK <121041290+KOOTHEMAVERICK@users.noreply.github.com>
2025-01-14 10:22:15 +0900

    test_cd6
    ..

--- infraScript/zero_downtime_deploy.py
+++ infraScript/zero_downtime_deploy.py
@@ -13,8 +13,8 @@ class ServiceManager:
         self.socat_port: int = socat_port
         self.sleep_duration: int = sleep_duration
         self.services: Dict[str, int] = {
-            '${blog}_1': 8082,
-            '${blog}_2': 8083
+            'blog_1': 8082,
+            'blog_2': 8083
         }
         self.current_name: Optional[str] = None
         self.current_port: Optional[int] = None
@@ -26,7 +26,7 @@ class ServiceManager:
         cmd: str = f"ps aux | grep 'socat -t0 TCP-LISTEN:{self.socat_port}' | grep -v grep | awk '{{print $NF}}'"
         current_service: str = subprocess.getoutput(cmd)
         if not current_service:
-            self.current_name, self.current_port = '${blog}_2', self.services['${blog}_2']
+            self.current_name, self.current_port = 'blog_2', self.services['blog_2']
         else:
             self.current_port = int(current_service.split(':')[-1])
             self.current_name = next((name for name, port in self.services.items() if port == self.current_port), None)
@@ -46,7 +46,7 @@ class ServiceManager:
     # Docker 컨테이너를 실행하는 함수
     def _run_container(self, name: str, port: int) -> None:
         os.system(
-            f"docker run -d --name={name} --restart unless-stopped -p {port}:${http://172.17.0.1:8081} -e TZ=Asia/Seoul -v /dockerProjects/${blog}/volumes/gen:/gen --pull always ghcr.io/${koothemaverick}/${blog}")
+            f"docker run -d --name={name} --restart unless-stopped -p {port}:${http://172.17.0.1:8081} -e TZ=Asia/Seoul -v /dockerProjects/blog/volumes/gen:/gen --pull always ghcr.io/KOOTHEMAVERICK/blog")
 
     def _switch_port(self) -> None:
         # Socat 포트를 전환하는 함수
