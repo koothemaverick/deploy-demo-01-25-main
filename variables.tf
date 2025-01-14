ad3f7398d8d57bb0d143c6e130c471608e876f85
KOOTHEMAVERICK <121041290+KOOTHEMAVERICK@users.noreply.github.com>
2025-01-14 15:38:17 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,9 @@
 variable "prefix" {
   description = "Prefix for all resources"
-  default     = "dev"
+  default     = "dev-" # <- 본인 네임번호로 수정 ex) dev-1
 }
 
 variable "region" {
   description = "region"
   default     = "ap-northeast-2"
-}
-
-variable "nickname" {
-  description = "nickname"
-  default     = "sik2"
 }
