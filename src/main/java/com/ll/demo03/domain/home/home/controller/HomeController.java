5a2b7a96450a88f9f03e0f06ec58f23b25265066
KOOTHEMAVERICK <121041290+KOOTHEMAVERICK@users.noreply.github.com>
2025-01-14 12:06:47 +0900

    cicd

--- src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "Hellooooo, World!!ss123123!!!, on " + siteName;
+        return "Hellooooo, World!!@!@!#!!!, on " + siteName;
     }
 
     @GetMapping("/secretKey")
